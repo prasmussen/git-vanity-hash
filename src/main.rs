0000149133399cd4516822764b3d7622bf603dce
Petter Rasmussen <petter@hask.no>
2020-02-22 11:04:25 +0100

    Add CommandError type

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io::Write;
 use std::string;
 use std::fmt;
 use std::env;
+use std::process;
 use std::process::Command;
 use std::process::Stdio;
 use crypto::digest::Digest;
@@ -12,22 +13,11 @@ use crypto::sha1::Sha1;
 
 enum Error {
     FailedToParseArgs(),
-    FailedToGetCommitInfo(io::Error),
-    FailedToReadCommitInfoStdout(string::FromUtf8Error),
-    FailedToReadCommitInfoStderr(string::FromUtf8Error),
-    GetCommitInfoNonZeroExitCode(String),
     FailedToParseCommitInfo(),
     PrefixNotFound(),
-    FailedToSpawnGitHashObject(io::Error),
-    FailedToCaptureStdinOfGitHashObject(),
-    GitHashObjectFailedToWriteStdin(io::Error),
-    FailedToWaitForGitHashObject(io::Error),
-    FailedToReadGitHashObjectStdout(string::FromUtf8Error),
-    FailedToReadGitHashObjectStderr(string::FromUtf8Error),
-    GitHashObjectNonZeroExitCode(String),
-    FailedToUpdateRef(io::Error),
-    FailedToReadUpdateRefStderr(string::FromUtf8Error),
-    UpdateRefNonZeroExitCode(String),
+    GitCatFile(CommandError),
+    GitHashObject(CommandError),
+    GitUpdateRef(CommandError),
 }
 
 
@@ -37,7 +27,7 @@ fn main() {
             (),
 
         Err(err) =>
-            print_err(err),
+            println!("{}", format_error(err)),
     };
 }
 
@@ -46,35 +36,43 @@ fn run(args: std::env::Args) -> Result<(), Error> {
     let options = Options::from_args(args)
         .ok_or(Error::FailedToParseArgs())?;
 
-    let commit_info_str = git_cat_file()?;
+    let commit_info_str = git_cat_file()
+        .and_then(process_command_output)
+        .map_err(Error::GitCatFile)?;
+
     let commit_info = CommitInfo::from_str(&commit_info_str)
         .ok_or(Error::FailedToParseCommitInfo())?;
 
     let vanity_commit_info = find_vanity_commit_info(&commit_info, &options.wanted_prefix)?;
+    let hash = vanity_commit_info.hash();
+
+    println!("Found hash: {}", hash);
 
     match options.mode {
-        Mode::Simulate() => {
-            println!("Found hash: {}", vanity_commit_info.hash());
-            Ok(())
-        },
+        Mode::Simulate() =>
+            (),
 
         Mode::Write() => {
-            let hash = git_hash_object(&vanity_commit_info.to_string())?;
-            println!("Found hash: {}", vanity_commit_info.hash());
-            git_update_ref(&hash)?;
-            println!("Commit updated");
-            Ok(())
+            git_hash_object(&vanity_commit_info.to_string())
+                .and_then(process_command_output)
+                .map_err(Error::GitHashObject)?;
+
+            git_update_ref(&hash)
+                .and_then(process_command_output)
+                .map_err(Error::GitUpdateRef)?;
+
+            println!("Commit updated")
         },
     }
+
+    Ok(())
 }
 
 
 fn find_vanity_commit_info(commit_info: &CommitInfo, wanted_prefix: &str) -> Result<CommitInfo, Error> {
-    let vanity_header = "vanity";
-
     for n in 0..std::u32::MAX {
         let vanity_value = format!("{:x}", n);
-        let vanity_commit_info = commit_info.add_header(&vanity_header, &vanity_value);
+        let vanity_commit_info = commit_info.add_header("vanity", &vanity_value);
         let hash = vanity_commit_info.hash();
 
         if hash.starts_with(wanted_prefix) {
@@ -85,134 +83,115 @@ fn find_vanity_commit_info(commit_info: &CommitInfo, wanted_prefix: &str) -> Res
     Err(Error::PrefixNotFound())
 }
 
-
-fn git_cat_file() -> Result<String, Error> {
-    let output = Command::new("git")
-        .arg("cat-file")
-        .arg("commit")
-        .arg("HEAD")
-        .output()
-        .map_err(Error::FailedToGetCommitInfo)?;
-
-    if output.status.success() {
-        String::from_utf8(output.stdout)
-            .map_err(Error::FailedToReadCommitInfoStdout)
-    } else {
-        let stderr = String::from_utf8(output.stderr)
-            .map_err(Error::FailedToReadCommitInfoStderr)?;
-
-        Err(Error::GetCommitInfoNonZeroExitCode(stderr))
-    }
+enum CommandError {
+    FailedToExecute(io::Error),
+    FailedToReadStdout(string::FromUtf8Error),
+    FailedToReadStderr(string::FromUtf8Error),
+    ExitFailure(String, Option<i32>),
+    FailedToCaptureStdin(),
+    FailedToWriteStdin(io::Error),
+    FailedToWaitForChild(io::Error),
 }
 
+fn git_cat_file() -> Result<process::Output, CommandError> {
+    Command::new("git").args(&["cat-file", "commit", "HEAD"])
+        .output()
+        .map_err(CommandError::FailedToExecute)
+}
 
-fn git_hash_object(commit_info_str: &str) -> Result<String, Error> {
+fn git_update_ref(hash: &str) -> Result<process::Output, CommandError> {
+    Command::new("git")
+        .args(&["update-ref", "HEAD", hash])
+        .output()
+        .map_err(CommandError::FailedToExecute)
+}
+
+fn git_hash_object(commit_info_str: &str) -> Result<process::Output, CommandError> {
     let mut child = Command::new("git")
-        .arg("hash-object")
-        .arg("-t")
-        .arg("commit")
-        .arg("-w")
-        .arg("--stdin")
+        .args(&["hash-object", "-t", "commit", "-w", "--stdin"])
         .stdin(Stdio::piped())
         .stderr(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
-        .map_err(Error::FailedToSpawnGitHashObject)?;
+        .map_err(CommandError::FailedToExecute)?;
 
     child.stdin
         .as_mut()
-        .ok_or(Error::FailedToCaptureStdinOfGitHashObject())?
+        .ok_or(CommandError::FailedToCaptureStdin())?
         .write_all(commit_info_str.as_bytes())
-        .map_err(Error::GitHashObjectFailedToWriteStdin)?;
+        .map_err(CommandError::FailedToWriteStdin)?;
 
-    let output = child.wait_with_output()
-        .map_err(Error::FailedToWaitForGitHashObject)?;
+    child.wait_with_output()
+        .map_err(CommandError::FailedToWaitForChild)
+}
 
+
+fn process_command_output(output: process::Output) -> Result<String, CommandError> {
     if output.status.success() {
         String::from_utf8(output.stdout)
-            .map(|str| str.trim_end().to_string())
-            .map_err(Error::FailedToReadGitHashObjectStdout)
+            .map_err(CommandError::FailedToReadStdout)
     } else {
         let stderr = String::from_utf8(output.stderr)
-            .map_err(Error::FailedToReadGitHashObjectStderr)?;
+            .map_err(CommandError::FailedToReadStderr)?;
 
-        Err(Error::GitHashObjectNonZeroExitCode(stderr))
+        Err(CommandError::ExitFailure(stderr, output.status.code()))
     }
 }
 
 
-fn git_update_ref(hash: &str) -> Result<(), Error> {
-    let output = Command::new("git")
-        .arg("update-ref")
-        .arg("HEAD")
-        .arg(hash)
-        .output()
-        .map_err(Error::FailedToUpdateRef)?;
-
-    if output.status.success() {
-        Ok(())
-    } else {
-        let stderr = String::from_utf8(output.stderr)
-            .map_err(Error::FailedToReadUpdateRefStderr)?;
-
-        Err(Error::UpdateRefNonZeroExitCode(stderr))
-    }
-}
-
-
-fn print_err(err: Error) {
+fn format_error(err: Error) -> String {
     match err {
         Error::FailedToParseArgs() =>
-            println!("Usage: <simulate|write> <wanted_prefix>"),
-
-        Error::FailedToGetCommitInfo(err) =>
-            println!("Failed to get commit info: {}", err),
-
-        Error::FailedToReadCommitInfoStdout(err) =>
-            println!("Failed to read commit info stdin as utf-8: {}", err),
-
-        Error::FailedToReadCommitInfoStderr(err) =>
-            println!("Failed to read commit info stderr as utf-8: {}", err),
-
-        Error::GetCommitInfoNonZeroExitCode(err) =>
-            println!("Got non-zero exit code when getting commit info: {}", err),
+            "Usage: <simulate|write> <wanted_prefix>".to_string(),
 
         Error::FailedToParseCommitInfo() =>
-            println!("Failed to parse commit info"),
+            "Failed to parse commit info".to_string(),
 
         Error::PrefixNotFound() =>
-            println!("Prefix not found"),
+            "Prefix not found".to_string(),
 
-        Error::FailedToSpawnGitHashObject(err) =>
-            println!("Failed to spawn git hash-object: {}", err),
+        Error::GitCatFile(err) =>
+            format!("git cat-file failed: {}", format_command_error(err)),
 
-        Error::FailedToCaptureStdinOfGitHashObject() =>
-            println!("Failed to capture stdin of git hash-object"),
+        Error::GitUpdateRef(err) =>
+            format!("git update-ref failed: {}", format_command_error(err)),
 
-        Error::GitHashObjectFailedToWriteStdin(err) =>
-            println!("Failed to write to stdin of git hash-object: {}", err),
+        Error::GitHashObject(err) =>
+            format!("git hash-object failed: {}", format_command_error(err)),
+    }
+}
 
-        Error::FailedToWaitForGitHashObject(err) =>
-            println!("Failed to wait for git hash-object: {}", err),
 
-        Error::FailedToReadGitHashObjectStdout(err) =>
-            println!("Failed to read stdout from git hash-object as utf-8: {}", err),
+fn format_command_error(err: CommandError) -> String {
+    match err {
+        CommandError::FailedToExecute(err) =>
+            format!("Failed to execute command: {}", err),
 
-        Error::FailedToReadGitHashObjectStderr(err) =>
-            println!("Failed to read stdout from git hash-object as utf-8: {}", err),
+        CommandError::FailedToReadStdout(err) =>
+            format!("Failed decode stdout as utf-8: {}", err),
 
-        Error::GitHashObjectNonZeroExitCode(err) =>
-            println!("Got non-zero exit code when running git hash-object: {}", err),
+        CommandError::FailedToReadStderr(err) =>
+            format!("Failed decode stderr as utf-8: {}", err),
 
-        Error::FailedToUpdateRef(err) =>
-            println!("Failed to update ref: {}", err),
+        CommandError::ExitFailure(stderr, exit_status) => {
+            match exit_status {
+                Some(code) =>
+                    format!("Exited with status code: {}\n{}", code, stderr),
 
-        Error::FailedToReadUpdateRefStderr(err) =>
-            println!("Failed to ref stderr from git update-ref as utf-8: {}", err),
+                None =>
+                    format!("Process terminated by signal\n{}", stderr),
+            }
+        },
 
-        Error::UpdateRefNonZeroExitCode(err) =>
-            println!("Got non-zero exit code when running git update-ref: {}", err),
-    };
+        CommandError::FailedToCaptureStdin() =>
+            "Failed to capture stdin".to_string(),
+
+        CommandError::FailedToWriteStdin(err) =>
+            format!("Failed to write to stdin: {}", err),
+
+        CommandError::FailedToWaitForChild(err) =>
+            format!("Failed to wait for child process: {}", err),
+    }
 }
 
 
